2027667c9b007099478f122c803b9f914e80dc3f
xushengyuano <1@>
2019-01-10 10:24:43 +0800

    123

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 header("Access-Control-Allow-Origin : http://10.30.29.83:63341");
 header("Access-Control-Allow-Credentials : true");
 header('Access-Control-Allow-Headers : Content-Type');
-echo '{"userName":"123"}';
+echo '{"userName":"12345"}';
+
 ?>
 
 
